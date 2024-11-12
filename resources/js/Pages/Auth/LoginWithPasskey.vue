485c13ee8b018180869fd32186c79bc959c08492
Mathew Paret <mathewparet@gmail.com>
2024-11-12 14:10:32 +1100

    LoginWithPasskey view updated

--- resources/js/Pages/Auth/LoginWithPasskey.vue
+++ resources/js/Pages/Auth/LoginWithPasskey.vue
@@ -9,6 +9,7 @@ import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
 import { ref } from "vue";
 import ConfirmsPasskey from '@/Components/ConfirmsPasskey.vue';
+import SectionBorder from '@/Components/SectionBorder.vue';
 
 defineProps({
     canResetPassword: Boolean,
@@ -22,7 +23,7 @@ const passkeyConfirmation = ref(null);
 const passwordInput = ref(null);
 
 const form = useForm({
-    __USERNAME__: '',
+    email: '',
     password: '',
     passkey: '',
     remember: false,
@@ -36,9 +37,14 @@ const submit = () => {
     else
     {
         isProcessing.value = true;
-        passkeyConfirmation.value.start(form.__USERNAME__);
+        passkeyConfirmation.value.start(form.email);
     }
 };
+
+const passkeyLogin = () => {
+    isProcessing.value = true;
+    passkeyConfirmation.value.start(form.email);
+}
 
 const passwordLogin = () => {
     form.transform(data => ({
@@ -71,23 +77,23 @@ const cancelPasskeyLogin = (failed) => {
             {{ status }}
         </div>
 
-        <form @submit.prevent="submit">
+        <form>
             <div>
-                <InputLabel for="__USERNAME__" value="__USERNAME_LABEL__" />
+                <InputLabel for="email" value="Email" />
                 <TextInput
-                    id="__USERNAME__"
-                    v-model="form.__USERNAME__"
-                    type="__USERNAME_TYPE__"
+                    id="email"
+                    v-model="form.email"
+                    type="email"
                     class="mt-1 block w-full"
                     required
                     autofocus
                     autocomplete="username"
                 />
-                <InputError class="mt-2" :message="form.errors.__USERNAME__" />
-                <InputError class="mt-2" :message="passkeyConfirmation?.passkeyForm?.errors?.__USERNAME__" />
+                <InputError class="mt-2" :message="form.errors.email" />
+                <InputError class="mt-2" :message="passkeyConfirmation?.passkeyForm?.errors?.email" />
             </div>
 
-            <div class="mt-4" v-if="isPasswordLogin">
+            <div class="mt-4">
                 <InputLabel for="password" value="Password" />
                 <TextInput
                     id="password"
@@ -108,20 +114,31 @@ const cancelPasskeyLogin = (failed) => {
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
+            <div class="flex items-center justify-center mt-4">
                 <Link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
                     Forgot your password?
                 </Link>
-                <span class="ml-3 text-sm">or</span>
+                <span class="text-sm text-gray-600 dark:text-gray-400 px-2">|</span>
                 <Link v-if="canResetPassword" :href="route('register')" class="ml-3 underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
                     Register
                 </Link>
 
-                <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing || isProcessing }" :disabled="form.processing || isProcessing">
-                    {{ isPasswordLogin ? 'Login' : 'Next' }}
-                </PrimaryButton>
             </div>
+            <PrimaryButton @click.prevent="passwordLogin" class="w-full mt-2 justify-center" :class="{ 'opacity-25': form.processing || isProcessing }" :disabled="form.processing || isProcessing">
+                  Login
+            </PrimaryButton>
+            <div class="flex items-center justify-center my-2">
+                <div class="flex-1 border-t border-gray-300"></div>
+                <span class="px-2 text-gray-500">or</span>
+                <div class="flex-1 border-t border-gray-300"></div>
+            </div>
+            <PrimaryButton @click.prevent="passkeyLogin" class="w-full justify-center" :class="{ 'opacity-25': form.processing || isProcessing }" :disabled="form.processing || isProcessing">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 mr-2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M7.864 4.243A7.5 7.5 0 0 1 19.5 10.5c0 2.92-.556 5.709-1.568 8.268M5.742 6.364A7.465 7.465 0 0 0 4.5 10.5a7.464 7.464 0 0 1-1.15 3.993m1.989 3.559A11.209 11.209 0 0 0 8.25 10.5a3.75 3.75 0 1 1 7.5 0c0 .527-.021 1.049-.064 1.565M12 10.5a14.94 14.94 0 0 1-3.6 9.75m6.633-4.596a18.666 18.666 0 0 1-2.485 5.33" />
+                </svg>
+                Login with Passkey
+            </PrimaryButton>
+            <ConfirmsPasskey :email="form.email" ref="passkeyConfirmation" @cancelled="cancelPasskeyLogin" mode="login" :remember="form.remember"/>
         </form>
-        <ConfirmsPasskey :__USERNAME__="form.__USERNAME__" ref="passkeyConfirmation" @cancelled="cancelPasskeyLogin" mode="login" :remember="form.remember"/>
     </AuthenticationCard>
 </template>
